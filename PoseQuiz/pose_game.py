b8787a35d2e5f413ebb45622bce027c827884c30
LeBronJames4me <44600736+LeBronJames4me@users.noreply.github.com>
2021-07-04 21:34:40 +0900

    Pose game UI interface
    rough design for pose UI interface

--- PoseQuiz/pose_game.py
+++ PoseQuiz/pose_game.py
@@ -21,15 +21,15 @@ class UIApp(QWidget):
 
         # 이미지 오브젝트 받아옴 (딕셔너리 형태) 초기값은 신서유기8 페이지
 
-        self.obj = QPixmap('img\pose_intro')
-        self.obj = self.obj.scaledToWidth(1500)
+        obj = QPixmap('img\pose_intro')
+        obj = obj.scaledToWidth(1500)
         self.img_label = QLabel()
-        self.img_label.setPixmap(self.obj)
+        self.img_label.setPixmap(obj)
         self.img_label.setStyleSheet("background-color : transparent")
         self.img_label.setAlignment(Qt.AlignCenter)
 
         self.lcd = QLCDNumber()
-        self.lcd.setDigitCount(1)
+        self.lcd.setDigitCount(2)
         self.lcd.setFixedWidth(200)
         self.lcd.setFixedHeight(100)
         self.lcd.setStyleSheet("""QLCDNumber {background-color: red;}""")
@@ -37,18 +37,34 @@ class UIApp(QWidget):
         self.timer = QTimer(self)
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.timeout)
-        self.count=0
+        #self.count=2
 
         # UI layout
-        self.hbox = QHBoxLayout()
-        self.hbox.addWidget(self.img_label)
+        self.vbox = QVBoxLayout()
 
+        self.hbox = QHBoxLayout()
+
+        self.title = QLabel('title')
+        self.title.setFont(QFont('Arial', 50))
+        self.title.setAlignment(Qt.AlignHCenter)
+        self.title.setVisible(False)
+
+        self.desc = QLabel('description')
+        self.desc.setFont(QFont('Arial', 50))
+        self.desc.setAlignment(Qt.AlignHCenter)
+        self.desc.setVisible(False)
+
+        self.vbox.addWidget(self.title)
+        self.vbox.addWidget(self.img_label)
+        self.vbox.addWidget(self.desc)
+
+        self.hbox.addLayout(self.vbox)
         self.setLayout(self.hbox)
 
         self.setWindowTitle('posing')
         #self.move(400, 50)
 
-        self.show()
+        self.showFullScreen()
 
 
     #입력 event 처리
@@ -56,21 +72,46 @@ class UIApp(QWidget):
         #입력 상황
         if e.key() == Qt.Key_Space:
 
-            #self.obj = QPixmap('img/pose_pic.png')
-            #self.obj = self.obj.scaledToHeight(900)
-            #self.img_label.setPixmap(self.obj)
-            self.img_label.setText("asdasdasdas")
+            self.count = 2
+            self.title.setVisible(False)
+            txt= "자유의 여신상"
+            self.img_label.setText(txt)
+            self.img_label.setFont(QFont('Arial', 75))
 
-            self.lcd.display(1)
+            self.desc.setVisible(True)
+            self.desc.setText('포즈를 취해주세요!')
             self.hbox.addWidget(self.lcd, 0, Qt.AlignTop)
+
+
+            self.timer.start()
             self.repaint()
 
+        elif e.key() == Qt.Key_Backspace:
+            self.close()
     def timeout(self):
         sender = self.sender()
-        #if id(sender) == id(self.timer):
+        self.count-=1
+        self.lcd.display(self.count)
 
+        if self.count==0:
+            self.timer.stop()
 
+            self.desc.setVisible(False)
+            self.title.setVisible(True)
+            self.title.setText('정답공개')
 
+            obj = QPixmap('img\pose_answer.png')
+            obj = obj.scaledToWidth(1500)
+            self.img_label.setPixmap(obj)
+
+            self.repaint()
+
+            time.sleep(2)
+
+            obj = QPixmap('img\pose_pic.png')
+            obj = obj.scaledToWidth(1500)
+            self.img_label.setPixmap(obj)
+            self.title.setText('촬영사진')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
