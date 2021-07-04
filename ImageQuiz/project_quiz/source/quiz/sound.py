7cbac21e1cb06f6cc026edc1fe56688eacb43f25
LifeIsOneShot <jyh1697@gmail.com>
2021-07-04 12:47:12 +0900

    feat: 게임 재시작 구현

--- ImageQuiz/project_quiz/source/quiz/sound.py
+++ ImageQuiz/project_quiz/source/quiz/sound.py
@@ -14,22 +14,20 @@ import MicrophoneStream as MS
 RATE = 16000
 CHUNK = 512
 
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-def py_error_handler(filename, line, function, err, fmt):
-  dummy_var = 0
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so')
-asound.snd_lib_error_set_handler(c_error_handler)
-
-
 
 	
 def correctSound():
+
 	with MS.MicrophoneStream(RATE, CHUNK) as stream:
-		audio_generator = stream.generator()
+		#audio_generator = stream.generator()
 		MS.play_file("audios/correct.wav") 
-	
+
 def wrongSound():
+
 	with MS.MicrophoneStream(RATE, CHUNK) as stream:
-		audio_generator = stream.generator()
+		#audio_generator = stream.generator()
 		MS.play_file("audios/wrong.wav")
+
+		
+#while 1:
+#	correctSound()
